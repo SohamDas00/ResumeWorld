2c246718cdffb3b882de73c8a9c3083fdff6c2f1
Soham Das <jisusoham04@gmail.com>
2026-02-02 23:53:45 +0530

    complete home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
           <Route path='builder/:resumeId' element={<ResumeBuilder/>}/>
         </Route>
         <Route path='view/:resumeId' element={<Preview/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/login'  element={<Login/>}/>
       </Routes>
     </>
   )
