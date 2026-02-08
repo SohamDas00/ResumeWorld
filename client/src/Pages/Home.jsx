b3a35b655551c09a951f6a1fb6ddd21867383474
Soham Das <jisusoham04@gmail.com>
2026-02-08 22:35:18 +0530

    update home and layout page

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Banner from '../Components/home/Banner'
+import Hero from '../Components/home/Hero'
+import Features from '../Components/home/Features'
+import Testimonial from '../Components/home/Testimonial'
+import About from '../Components/home/About'
+import Footer from '../Components/home/Footer'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Banner/>
+      <Hero/>
+      <About/>
+      <Features/>
+      <Testimonial/>
+      <Footer/>
+    </div>
   )
 }
 
